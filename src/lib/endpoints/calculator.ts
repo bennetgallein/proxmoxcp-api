ff59a32180c66279f98eb8fb2f79ee1daa41d928
Bennet Gallein <me@bennetgallein.de>
2024-05-19 15:56:01 +0200

    feat: init

---
+++ src/lib/endpoints/calculator.ts
@@ -0,0 +1,33 @@
+import { XiorInstance } from "xior";
+import { DataProvider } from "../common/data.provider";
+import { CalculationRequest, CalculationResponse, CalculatorOptionsResponse } from "../types/calculator.types";
+
+export class Calculator extends DataProvider {
+  constructor(readonly client: XiorInstance) {
+    super(client);
+  }
+
+  /**
+   * get a list of available cpu/ram/disk options for the provided template
+   *
+   * @param templateVmId the VMID of the tempalte, NOT the id
+   * @param charges array of charge IDs that you want to include into the calculation
+   * @returns
+   */
+  public async getOrderOptions(templateVmId?: string, charges?: number[]) {
+    return this.client.post<CalculatorOptionsResponse>(`/api/order/get-specs`, {
+      os: templateVmId,
+      options: JSON.stringify(charges)
+    });
+  }
+
+  /**
+   * calculate the price of a server
+   *
+   * @param request
+   * @returns
+   */
+  public async calculate(request: CalculationRequest) {
+    return this.client.post<CalculationResponse>(`/api/order/calc`, request);
+  }
+}
---
+++ src/lib/types/calculator.types.ts
@@ -0,0 +1,44 @@
+export type CalculatorOptionsResponse = {
+  cpu: CalculatorOptionItem[];
+  ram: CalculatorOptionItem[];
+  disk: CalculatorOptionItem[];
+}
+
+export type CalculatorOptionItem = {
+  value: number;
+  text: string;
+  selected: boolean
+}
+
+export type CalculationRequest = {
+  cpu: number;
+  ram: number;
+  disk: number;
+  os: number;
+  voucher: string;
+  options: number[];
+}
+
+export type CalculationResponse = {
+  rawprice: string;
+  price: string;
+  charges: Charge[];
+  hasCreationTimes: boolean;
+  creationTime: false | string;
+  _creationTime: false | number;
+  calc: Pick<CalculationRequest, 'cpu' | 'ram' | 'disk'>,
+  configurableCharges: Charge[]
+}
+
+export type Charge = {
+  id: number,
+  price: string;
+  type: number;
+  calcType: number;
+  calcOnly: number;
+  osid: number | null;
+  recurring: number;
+  description: string;
+  active: number;
+  descriptionRaw: string;
+}
